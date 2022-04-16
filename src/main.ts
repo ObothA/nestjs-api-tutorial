0aecf89286800bfbd29c2a0257e00488ba1666d9
obotha <oboth.abel.oa@gmail.com>
2022-04-16 12:24:40 +0300

    Add whitelisting in the validator.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // allows use validation pipes globally
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  // white list strips out un wanted paramters.
+
   await app.listen(3333);
 }
 bootstrap();
