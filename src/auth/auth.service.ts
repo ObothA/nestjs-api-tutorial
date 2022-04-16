a2377c6a5c74146a992c3f45f354c4212d5151e4
obotha <oboth.abel.oa@gmail.com>
2022-04-16 15:18:24 +0300

    Throw forbidden error.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import * as argon from 'argon2';
 
 import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDto) {
-    // generate the password hash
-    const hash = await argon.hash(dto.password);
+    try {
+      // generate the password hash
+      const hash = await argon.hash(dto.password);
 
-    // save new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+      // save new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    // return the saved user
-    return user;
+      delete user.hash;
+
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          // unique field violation
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
+      }
+
+      throw error;
+    }
   }
 
   signin() {
